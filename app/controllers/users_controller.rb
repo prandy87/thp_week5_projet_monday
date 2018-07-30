1280ad92981f174411304c00bee234d6d2fd80f4
christopheleray <christophe.leray1@gmail.com>
2018-07-30 15:12:06 +0200

    Enregisremet  user params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,17 @@ class UsersController < ApplicationController
   def new
   	@random_number = rand(2000)
   	puts "random_number = #{@random_number}"
+    puts "on affiche depuis def new"
   end
+
+  def create
+    @random_number = rand(2000)
+    puts params
+    puts "Token: #{params[:authenticity_token]}"
+    puts "random_number create= #{@random_number}"
+    user = User.create(username: params[:username] , bio: params[:bio])
+    puts user.username
+    puts "on affiche depuis create "
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about', as: 'about'
   get '/about/:user', to: 'static_pages#about_user', as: 'about_user'
   get '/about/:group', to: 'static_pages#about_group', as: 'about_group'
+  post '/new', to: 'users#create', as: 'create_user'
 end
