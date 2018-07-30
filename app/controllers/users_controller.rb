267a98b078dbea4524da19f7550d489845484391
christopheleray <christophe.leray1@gmail.com>
2018-07-30 16:44:33 +0200

    ajout view show en cours

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,10 +10,16 @@ class UsersController < ApplicationController
     puts params
     puts "Token: #{params[:authenticity_token]}"
     puts "random_number create= #{@random_number}"
-    user = User.create(username: params[:username] , bio: params[:bio])
-    puts user.username
+    @user = User.create(username: params[:username] , bio: params[:bio])
+    puts @user.username
     puts "on affiche depuis create "
-    redirect_to'/'
+    redirect_to '/'
+  end
+
+  def show
+    puts "j'affiche depuis show"
+    puts params[:id]
+    @user = User.last
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/new'
   root to: 'static_pages#home', as: 'home'
   get '/new', to: 'users#new', as: 'new_user'
+  post '/create', to: 'users#create', as: 'create_user'
   get '/contact', to: 'static_pages#contact', as: 'contact'
   get '/about', to: 'static_pages#about', as: 'about'
-  get '/about/:user', to: 'static_pages#about_user', as: 'about_user'
-  get '/about/:group', to: 'static_pages#about_group', as: 'about_group'
-  post '/new', to: 'users#create', as: 'create_user'
+  get '/users/:id', to: 'users#show', as: 'show'
 end
