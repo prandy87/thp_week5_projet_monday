d3b081d69b2d6a3c3c43d77eb4a613e385f0e784
Ertemelle <phenixime@hotmail.fr>
2018-07-30 14:27:35 +0200

    creation de user_new

---
+++ app/helpers/users_cotroller_helper.rb
@@ -0,0 +1,2 @@
+module UsersCotrollerHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/new'
   root to: 'static_pages#home', as: 'home'
+  get '/new', to: 'users#new', as: 'new_user'
   get '/contact', to: 'static_pages#contact', as: 'contact'
   get '/about', to: 'static_pages#about', as: 'about'
   get '/about/:user', to: 'static_pages#about_user', as: 'about_user'
